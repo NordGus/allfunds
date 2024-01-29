21f04d7e784c003b42bed3ef3c36aa93e82a0229
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-29 17:34:39 +0100

    adding Refund::Rules spec

---
+++ backend/spec/app/refund/rules_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe Refund::Rules do
+  describe "self.new_for" do
+    shared_examples :refund_rule do |rule_class, rule, args|
+      it "must return the expected rule instance" do
+        expect(Refund::Rules.new_for(rule: rule, args: args)).to be_instance_of(rule_class)
+      end
+    end
+
+    context "transportation" do
+      include_examples :refund_rule, [
+        Refund::Rules::TransportationRule,
+        "transportation",
+        { first_rate: 0.12, threshold: 100, second_rate: 0.08 }
+      ]
+    end
+
+    context "meal" do
+      include_examples :refund_rule, [
+        Refund::Rules::MealRule,
+        "meal",
+        { first_rate: 10, threshold: 3, second_rate: 6 }
+      ]
+    end
+
+    context "parking" do
+      include_examples :refund_rule, [
+        Refund::Rules::ParkingRule,
+        "parking",
+        { first_rate: 1, threshold: 20, second_rate: 0.5 }
+      ]
+    end
+  end
+end
