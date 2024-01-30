318f6a5adc5e541a547021cf7bd6a87acb4d66c5
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-30 09:08:11 +0100

    designing Preview component

---
+++ frontend/allfunds/src/components/Products/Preview.tsx
@@ -0,0 +1,23 @@
+import {PropsWithChildren} from "react";
+
+type Props = {
+    name: string,
+    price: string,
+    description: string,
+    imageUrl: string
+}
+
+export function Preview({ name, price, description, imageUrl, children }: PropsWithChildren<Props>) {
+    return (
+        <div className="grid grid-cols-2 grid-rows-4 gap-2">
+            <div className="col-span-2 flex justify-center items-center overflow-clip">
+                <img className="w-full" src={ imageUrl } alt={ name }/>
+            </div>
+            <h4>{name}</h4><p>{price}</p>
+            <p className="col-span-2">{description}</p>
+            <div className="col-span-2 flex justify-between">
+                { children }
+            </div>
+        </div>
+    )
+}
